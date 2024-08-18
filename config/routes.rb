0632693dbb13bcde99906c7fb543785d7c9c5fc1
mahichy <mohiuddinoshim@yahoo.com>
2024-08-18 14:19:53 +0600

    Update navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+root "pages#home"
 end
