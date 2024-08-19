5d3de4047a2455c7177a08626f661f1917f6b5d7
mahichy <mohiuddinoshim@yahoo.com>
2024-08-19 14:11:53 +0600

    change some basic and add graveter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def avatar_url(user)
+		
+		
+			gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+			"https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+		
+	end
 end
+
+# if user.image
+#   "http://graph.facebook.com/#{user.uid}/picture?type=large"
+# else
+# end
